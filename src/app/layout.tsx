db55b0894d9a54a056cbf63528af8edc6d76d8ab
Atuma David <atumadavid35@gmail.com>
2024-07-09 20:46:28 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <div className="flex flex-col min-h-screen w-full items-center dark:bg-black">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
