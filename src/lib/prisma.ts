55da9b2a665007569097b05a7e57073f0fec5f6a
Atuma David <atumadavid35@gmail.com>
2024-07-11 23:20:07 +0100

    collection form/server actions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,29 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
