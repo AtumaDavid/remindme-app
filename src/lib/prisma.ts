bcb20309ed8a66bc7edd47bb5376e5c28298a96b
Atuma David <atumadavid35@gmail.com>
2024-07-13 01:25:55 +0100

    postgres deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,29 +1,7 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClientSingleton | undefined;
-// };
-
-// export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
